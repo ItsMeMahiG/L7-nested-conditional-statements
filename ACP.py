7726c75e16bdcdc8f4c13a975a988c7539031135
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-10-20 08:35:59 +0530

    acp ke sath

--- ACP.py
+++ ACP.py
@@ -2,4 +2,4 @@ age=int(input("enter your age : "))
 if age<20 and age>10 :
     print ("you are allowed in the class")
 else :
-    print ("you are not allowed in the class")
+    print (" sorry, you are not allowed in the class")
